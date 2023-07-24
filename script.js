a4d2bacd4f2d65c75d0fae4f21906c4a3fb8cc45
Nick-132 <140430503+Nick-132@users.noreply.github.com>
2023-07-24 12:31:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function randomcolor() {
+    return Math.floor(Math.random() * 255);
+}
+function asdf() {
+  const dvd = document.getElementById("dvd");
+    dvd.style.backgroundColor =
+        rgba('+randomcolor()+','+randomcolor()+','+randomcolor()+'\)"
+}
