165f41baaa5f59c5fe4885af3b9fd76c0a930f29
Nick-132 <140430503+Nick-132@users.noreply.github.com>
2025-02-25 12:24:54 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function randomcolor() {
-    return Math.floor(Math.random() * 255);
-}
-function asdf() {
-  const dvd = document.getElementById("dvd");
-    dvd.style.backgroundColor =
-        rgba('+randomcolor()+','+randomcolor()+','+randomcolor()+'\)"
-}
